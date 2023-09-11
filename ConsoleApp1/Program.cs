75e725519616d2367aa8082a7ca72d08a3045b3f
BadPeopel1 <4B1G0127@stust.edu.tw>
2023-09-11 09:52:13 +0800

    n*n乘法表

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,16 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("輸入一個整數n");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine($"{i}*{j}={i * j}");
+            }
+            Console.WriteLine();
+        }
     }
 }
